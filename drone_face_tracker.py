64a466c9e4ad3ed43fd8920b9cc932bec85d0bbb
Fawwaz Anrico <fawwazanricopurnomo@gmail.com>
2023-09-15 15:57:51 +0700

    initial commit

---
+++ drone_face_tracker.py
@@ -0,0 +1,128 @@
+import cv2
+from djitellopy import tello
+import numpy as np
+import time
+
+drone = tello.Tello()
+
+
+drone.connect()
+
+
+fb_range = [20000, 22000]#sesuaikan dengan resolusi wajah yang dinginkan
+pid = [0.4 , 0.4 , 0.4]
+
+p_error = [0,0]
+
+cam_w = 640
+cam_h = 480
+
+
+def motion_movement(drone, feature, width, height, pid, prev_error):#parameter asli(drone, feature, camera weight, pid, previous error)
+
+    area = feature[1]
+    x, y = feature[0]
+    x_error = x - (width // 2)
+    y_error = y - (height // 2)
+    fb_speed = 0
+    yaw_speed = 0
+    ud_speed = 0
+    yaw_speed = pid[0]*x_error + pid[1]*(x_error - prev_error[0])
+    yaw_speed = int(np.clip(yaw_speed,-100,100))
+    ud_speed = pid[0]*y_error + pid[1]*(y_error - prev_error[1])
+    ud_speed = int(np.clip(ud_speed, -100,100))
+
+    # if x_error < 0:
+    #     yaw_speed = 20
+    #     print("slowly turn left :", abs(yaw_speed))
+    # elif x_error > 0:
+    #     yaw_speed = -20
+    #     print("slowly turn right :", yaw_speed)
+
+
+    if area > fb_range[0] and area < fb_range[1]:
+        fb_speed = 0
+        print("Stay")
+    elif area < fb_range[0]:
+        fb_speed = 10
+        print("move forward 15 cm")
+    else:
+        fb_speed = -10
+        print("move backward 15 cm")
+
+    if feature[1] != 0:
+        #drone.send_rc_control(0, fb_speed, -ud_speed, yaw_speed)
+        drone.send_rc_control(0, 0, 0, yaw_speed)
+    else:
+        drone.send_rc_control(0,0,0,0)
+
+    return [x_error, y_error]
+
+
+def face_finder(img):
+    face_cascade = cv2.CascadeClassifier("trained_file/haarcascade_frontalface_default.xml")
+    #ubah jadi gray background
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #labelling x,y,w,h tiap wajah
+    faces = face_cascade.detectMultiScale(img_gray, 1.3, 4)
+    face_list = list()
+    face_area = list()
+    for x,y,w,h in faces:
+        cv2.rectangle(img,(x,y), (x+w, y+h), (255, 0, 0), 5)
+        #calculating center of faces and its area
+        center_x = x + (w // 2)
+        center_y = y + (h // 2)
+        area = w * h
+        cv2.circle(img, (center_x, center_y),5,(0,0,255), cv2.FILLED)
+        face_list.append([center_x, center_y])
+        face_area.append(area)
+    
+    if len(face_list) != 0:
+        #if there is multiple faces, take the closest one
+        face_index = face_area.index(max(face_area))
+        return img, [face_list[face_index], face_area[face_index]]
+    else:
+        return img, [[0,0], 0]
+        
+def face_tracker():
+    global p_error
+    #capture = cv2.VideoCapture(0)
+    
+    while True:
+        #ret, img = capture.read()
+        img = drone.get_frame_read().frame
+        img = cv2.resize(img, (cam_w, cam_h))
+        img, feature = face_finder(img)
+        p_error = motion_movement(drone,feature, cam_w,cam_h, pid, p_error)
+        print("Center : ", feature[0], "Area : ", feature[1])
+        cv2.imshow("Output", img)
+        cv2.putText(img, f"Area: {feature[1]}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            drone.land()
+            break
+    #capture.release()
+    cv2.destroyAllWindows()
+    return
+
+if __name__ == '__main__':
+    batt = drone.get_battery()
+    print("Battery percentage: ", batt)
+    print("If u want to start press 's' on your keyboard :")
+    while True:
+        if input() == 's':
+            break
+    # start = time.time()
+    # end = time.time()
+
+    # while end - start < 5:
+    #     end = time.time()
+
+    drone.takeoff()
+    #drone.send_rc_control(0,0 ,15 ,0)
+    print('Start tracking? (press y): ')
+    while True:
+        if input() == 'y':
+            drone.streamon()
+            face_tracker()
+            break
+    
---
+++ hand_control.py
@@ -0,0 +1,145 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+from djitellopy import tello
+
+
+
+drone = tello.Tello()
+drone.connect()
+
+fb_range = [40000, 42000]
+pid = [0.4 , 0.4 , 0.4]
+p_error = [0,0]
+cam_w = 640
+cam_h = 480
+
+
+def hand_follow(drone, hand_bounding_area, center_point, prev_error):
+    area = hand_bounding_area
+    x, y = center_point
+    x_error = x - (cam_w // 2)
+    y_error = y - (cam_h // 2)
+    fb_speed = 0
+    yaw_speed = pid[0]*x_error + pid[1]*(x_error - prev_error[0])
+    yaw_speed = int(np.clip(yaw_speed,-100,100))
+    ud_speed = pid[0]*y_error + pid[1]*(y_error - prev_error[1])
+    ud_speed = int(np.clip(ud_speed, -100,100))
+
+    if area > fb_range[0] and area < fb_range[1]:
+        fb_speed = 0
+        print("Stay")
+    elif area < fb_range[0]:
+        fb_speed = 15
+        print("move forward 15 cm")
+    else:
+        fb_speed = -15
+        print("move backward 15 cm")
+
+    if area != 0:
+         drone.send_rc_control(0, fb_speed, -ud_speed, -yaw_speed)
+    else:
+         drone.send_rc_control(0,0,0,0)
+
+    return [x_error, y_error]
+
+def hand_gesture(hand_point_list, bounding_area, bounding_center):
+    global p_error
+    thumbs_open = True
+    index_open = True
+    middle_open = True
+    ring_open = True
+    pinky_open = True
+
+    # Check if all required landmarks are present
+    required_landmarks = [3, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+    if all(landmark in hand_point_list for landmark in required_landmarks):
+        if hand_point_list[4][0] > hand_point_list[3][0]:
+            thumbs_open = False
+        if hand_point_list[8][1] > hand_point_list[6][1]:
+            index_open = False
+        if hand_point_list[12][1] > hand_point_list[10][1]:
+            middle_open = False
+        if hand_point_list[16][1] > hand_point_list[14][1]:
+            ring_open = False
+        if hand_point_list[20][1] > hand_point_list[18][1]:
+            pinky_open = False
+    
+
+    ##tinggal kesepakatan mau gerak gimana
+    if (index_open and middle_open) and (not thumbs_open and not ring_open and not pinky_open):
+        drone.flip_forward()
+        print("flip forward")
+    elif thumbs_open and index_open and middle_open and ring_open and pinky_open:
+        p_error = hand_follow(drone, bounding_area, bounding_center, p_error)
+        print("follow my hand")
+
+    elif not(thumbs_open or index_open or middle_open or ring_open or pinky_open):
+        drone.land()
+        print("landing")
+    else:
+        print("do nothing")
+    return [thumbs_open, index_open, middle_open, ring_open, pinky_open]
+    
+
+
+def cam_stream():
+    #cam = cv2.VideoCapture(0)
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpDraw = mp.solutions.drawing_utils
+    bounding_area = 0
+    bounding_center = list()
+    drone.streamon()
+    while True:
+        #success , img = cam.read()
+        img = drone.get_frame_read().frame
+        img = cv2.flip(img, 1)
+        imageRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imageRGB)
+        hand_point = dict()
+        if results.multi_hand_landmarks:
+            hand_detected = True
+            for idx, handLms in enumerate(results.multi_hand_landmarks): # working with each hand
+                
+                # Check if this is the right hand
+                if results.multi_handedness[idx].classification[0].label == 'Right':
+                    landmarks_list = []
+                    for id, lm in enumerate(handLms.landmark):
+                        h, w, c = img.shape
+                        cx, cy = int(lm.x * w), int(lm.y * h)
+                        hand_point[id]=[cx, cy]
+                        landmarks_list.append([cx, cy])  
+                        # store the coordinates in the dictionary
+                    mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+                    landmarks_array = np.array(landmarks_list, dtype=np.int32)
+                    x, y, w, h = cv2.boundingRect(landmarks_array)
+                    bounding_area = w * h
+                    bounding_center = [x + w // 2,y + h // 2]
+                    # Draw the bounding rectangle on the image
+                    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                    print("Center : ", bounding_center, "Area : ", bounding_area)
+                    
+        else:
+            hand_detected = False
+
+        if hand_detected:
+            gesture_result = hand_gesture(hand_point, bounding_area, bounding_center)
+        cv2.imshow("Output", img)
+        
+        cv2.waitKey(1)
+if __name__ == "__main__":
+    batt = drone.get_battery()
+    print("Battery percentage: ", batt)
+    print("If u want to start press 's' on your keyboard :")
+    while True:
+        if input() == 's':
+            break
+    
+    drone.takeoff()
+    drone.send_rc_control(0,0,10,0)
+    print('Start tracking? (press y): ')
+    while True:
+        if input() == 'y':
+            cam_stream()
+            break
